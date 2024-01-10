a2fa786666c9b29b6be358cc27a6291cc0bb6817
Alistair_Piot <alistair.pro15@gmail.com>
2024-01-10 15:09:04 +0100

    1.2 : ajout des boutons en js + animation

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,4 +1,5 @@
-// Fonction pour effectuer la requête Fetch et manipuler les données
+/*--- Step 1.1 : Retrieving jobs from the back-end ---*/
+// Function to perform the Fetch request and manipulate the data
 async function fetchProjects() {
     try {
         const response = await fetch("http://localhost:5678/api/works");
@@ -7,7 +8,7 @@ async function fetchProjects() {
         // Retrieving the gallery from the DOM
         const gallery = document.querySelector(".gallery");
 
-        // Parcourir les projets et ajouter chaque projet à la galerie
+        // Browse projects and add each project to the gallery
         projects.forEach((project) => {
             const projectElement = document.createElement("figure");
             const imgElement = document.createElement("img");
@@ -31,3 +32,32 @@ async function fetchProjects() {
 
 // Call the fetchProjects function when the page loads
 window.addEventListener("load", fetchProjects);
+
+/* ---------- NEXT STEP ----------*/
+/*--- Step 1.2 : Filtering the work ---*/
+// Retrieve the div element with the "group-buttons" class
+const groupButtonsDiv = document.getElementById("group-buttons");
+
+// Table of button contents
+const buttonLabels = ["Tous", "Objets", "Appartements", "Hôtels & restaurants"];
+
+// Loop to create and add buttons to the div
+for (let i = 0; i < buttonLabels.length; i++) {
+    let button = document.createElement("button");
+    // Add the "active" class to the first button
+    button.className = "btn" + (i === 0 ? " active" : "");
+    button.textContent = buttonLabels[i];
+    groupButtonsDiv.appendChild(button);
+
+    // Ajouter un gestionnaire d'événements pour le clic sur le bouton
+    button.addEventListener("click", function () {
+        // Supprimer la classe "active" de tous les boutons
+        const allButtons = groupButtonsDiv.getElementsByClassName("btn");
+        for (let j = 0; j < allButtons.length; j++) {
+            allButtons[j].classList.remove("active");
+        }
+
+        // Ajouter la classe "active" au bouton cliqué
+        this.classList.add("active");
+    });
+}
