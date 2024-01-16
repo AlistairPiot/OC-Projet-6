0e06bf62e28441df509f2c3a71cda5747e86869c
Alistair_Piot <alistair.pro15@gmail.com>
2024-01-16 22:38:07 +0100

    ajout de la redirection vers la page d'accueil lors du clic sur le bouton "se connecter"

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,5 +1,6 @@
 //*--- Step 2.2 : User authentication ---*//
 // Retrieve the form
+/*
 document
     .querySelector('.form-login input[type="submit"]')
     .addEventListener("click", function () {
@@ -8,5 +9,38 @@ document
         )) {
             input.setCustomValidity("hello");
             input.reportValidity();
+        }
+    });   
+*/
+
+document
+    .querySelector("#form-login")
+    .addEventListener("submit", function (event) {
+        // Prevent the form from being sent by default
+        event.preventDefault();
+
+        // Set the valid variable to true
+        let valid = true;
+
+        // Browse all input fields except the "submit" field
+        for (let input of this.querySelectorAll('input:not([type="submit"])')) {
+            // Check if the field is empty
+            if (input.value.trim() === "") {
+                valid = false;
+                break;
+            }
+
+            valid &= input.reportValidity();
+            if (!valid) {
+                break;
+            }
+        }
+
+        if (valid) {
+            alert("Votre login a bien été validé !");
+            // Redirect to index.html
+            window.location.href = "../../index.html";
+        } else {
+            alert("Veuillez remplir tous les champs correctement.");
         }
     });
